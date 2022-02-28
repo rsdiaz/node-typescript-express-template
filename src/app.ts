9a76c9a2241d8663a7e162adf5f5c49b7375cad5
rsdiaz <robertoserrano83@gmail.com>
2022-02-28 23:18:17 +0100

    fix morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
+import 'dotenv/config'
 import express from 'express'
-import compression from 'compression'
+// import compression from 'compression'
 import lusca from 'lusca'
 import morgan from 'morgan'
 import routes from './routes'
+import { PORT, ENVIRONMENT } from './util/secrets'
 
 const app = express()
+const prod = ENVIRONMENT === 'production'
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', PORT || 3000)
 // app.use(compression)
 app.use(express.json())
 app.use(lusca.xframe('SAMEORIGIN'))
 app.use(lusca.xssProtection(true))
-app.use(morgan('dev'))
+app.use(prod ? morgan('combined') : morgan('dev'))
 
 app.use(routes)
 
